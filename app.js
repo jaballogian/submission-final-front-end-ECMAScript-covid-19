a2e7c16ef91fe1ce94b3e3faf772eede072af7c7
jaballogian <jaballogianrma77@gmail.com>
2020-04-23 16:03:21 +0700

    add and configure webpack

--- app.js
+++
@@ -1,4 +0,0 @@
-import "/src/script/component/app-bar.js";
-import "/src/script/component/result-bar.js";
-import "/src/script/component/country-row.js";
-// import "/src/style/style.css";
---
+++ src/app.js
@@ -0,0 +1,4 @@
+import "./style/style.css";
+import "./script/component/app-bar.js";
+import "./script/component/result-bar.js";
+import "./script/component/country-row.js";

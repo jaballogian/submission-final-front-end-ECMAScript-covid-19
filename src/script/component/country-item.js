fde7937f2cee663fa368d564877b149514c10ced
jaballogian <jaballogianrma77@gmail.com>
2020-04-23 15:18:17 +0700

    try to add country list using custom element and shadow dom

---
+++ src/script/component/country-item.js
@@ -0,0 +1,39 @@
+class CountryItem extends HTMLElement {
+
+    constructor(){
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
+    // set country(country) {
+    //     this._country = country;
+    //     this.render();
+    // }
+
+    connectedCallback(){
+        this.render();
+    }
+
+    render() {
+        this.shadowDOM.innerHTML = `
+        <style>
+            .main-row {
+                display: flex;
+                background-color: dimgrey;
+                width: 100%;
+            }
+            
+            .main-row .main-cell {
+                width: 20%;
+                color: white;
+            }
+        </style>
+        <div class="main-row">
+            <div class="main-cell">
+                <h1>tes</h1>
+            </div>
+        </div>`;
+    }
+}
+
+customElements.define("country-item", CountryItem);
---
+++ src/script/component/country-row.js
@@ -0,0 +1,28 @@
+import "./country-item.js";
+
+class CountryRow extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+ 
+    // set rows(rows) {
+    //     this._rows = rows;
+    //     this.render();
+    // }
+
+    connectedCallback(){
+        this.render();
+    }
+
+    render() {
+        this.shadowDOM.innerHTML = ``;
+        for(let i = 0; i < 5; i++){
+            const contryItemElement = document.createElement("country-item");
+            this.shadowDOM.appendChild(contryItemElement);
+        }
+    }
+}
+
+customElements.define("country-row", CountryRow);
